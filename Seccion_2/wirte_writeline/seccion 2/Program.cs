d4b5b97875f37fc46a034fc360810cad4af20ad0
Carlos López <carlos.65337@gmail.com>
2021-02-28 14:16:52 -0500

    creamos dos secciones

---
+++ Seccion_2/wirte_writeline/seccion 2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace write_and_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Aca mostramos los pasos de un algoritmo
+             */
+
+
+            /*
+             
+            notacion cammello ===> sumaDeDosNumeros.
+            notacion pascal ===> SumaDeDosNumeros.
+             
+             */
+
+
+
+            Console.Write("1. Precalentar el horno");
+            Console.WriteLine(" 2. Mezclar harina y mantequilla");
+            Console.WriteLine("3. Agregar aceite y 1 huevo");
+            Console.WriteLine("4. Amasar mezcla");
+            Console.WriteLine("5. Darle forma y colocar en la charola");
+            Console.WriteLine("6. Hornear durante 10min");
+
+            
+
+        }
+    }
+}
