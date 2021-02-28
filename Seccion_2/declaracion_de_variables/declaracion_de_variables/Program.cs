10389008a7d443a63eff25887c228790f7858a17
Carlos López <carlos.65337@gmail.com>
2021-02-28 14:37:21 -0500

    Declarar Variables

---
+++ Seccion_2/declaracion_de_variables/declaracion_de_variables/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace declaracion_de_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /**
+             * 
+             * Declaracion de variables ==> su tipo y el nombre que se le va a dar
+             * 
+             * */
+
+            int numerosLibrosa ;
+            int librosNiñosb;
+            int librosMatematicasc;
+
+            //variables del mismo tipo
+            int numerosLibros, librosNiños, librosMatematicas;
+
+
+            double promedioFinal;
+
+
+
+
+
+
+
+
+             
+
+        }
+    }
+}
