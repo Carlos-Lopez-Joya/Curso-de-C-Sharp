ef2080a77d63a8258519696474cae4722ced66e5
Carlos López <carlos.65337@gmail.com>
2021-02-28 13:06:53 -0500

    HolaMundo

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+            /*
+             Declaracion de variables, Primer HOLAMUNDO
+             */
+            var a = "HOla";
+            var b = " Carlos";
+
+            a = "Hello";
+
+            Console.WriteLine(a+b);
+            Console.WriteLine(a+b);
+
+
+            b = " World!";
+            Console.WriteLine(a + b);
+
+            
+          
+      
+
+        }
+    }
+}
